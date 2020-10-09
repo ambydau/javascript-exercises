d0706d30060d388c7261a3d19e5724a5a7c09bfa
Amber <amberedaniels09@gmail.com>
2020-10-09 12:22:37 -0700

    repeated string exercise finished

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+      let repeatedString = "";
+      if(num < 0){
+            repeatedString = "ERROR";
+      }
+      while(num > 0) {
+            repeatedString += str;
+            num--;
+      }
+      return repeatedString;
 }
 
 module.exports = repeatString
