1c620d788379d98875e11fd8e847db9804e38157
Amber <amberedaniels09@gmail.com>
2020-10-09 12:30:27 -0700

    reverse string assignment finished

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+      let strArray = [...str];
+      let reversedStr = "";
+      for(let i = strArray.length - 1; i >= 0; i--){
+            reversedStr += strArray[i];
+      }
+      return reversedStr;
 }
 
 module.exports = reverseString
