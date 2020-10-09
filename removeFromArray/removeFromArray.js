cba733eededfbd8130ab34e35c015410317c13b2
Amber <amberedaniels09@gmail.com>
2020-10-09 14:50:16 -0700

    remove from array assignment completed

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    const array = args[0];
+    const newArray = array.filter(item => !args.includes(item));
+    return newArray;
 }
 
 module.exports = removeFromArray
