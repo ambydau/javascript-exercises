063ef60c66109c87217f4e18fd1530b0343a9bc5
Amber <amberedaniels09@gmail.com>
2020-10-09 15:01:27 -0700

    refactored remove from array

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,7 +1,12 @@
-const removeFromArray = function(...args) {
-    const array = args[0];
-    const newArray = array.filter(item => !args.includes(item));
-    return newArray;
+// const removeFromArray = function(...args) {
+//     const array = args[0];
+//     const newArray = array.filter(item => !args.includes(item));
+//     return newArray;
+// }
+
+const removeFromArray = function(array, ...values) {
+      const newArray = array.filter(item => !values.includes(item));
+      return newArray;
 }
 
 module.exports = removeFromArray
