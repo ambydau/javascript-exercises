255e02bf8df8c873bde3355448071d03d65f5699
Amber <amberedaniels09@gmail.com>
2020-10-09 15:49:06 -0700

    temp conversions assignment completed

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+// (F - 32)*5/9 = C
+  let tempC = (tempF - 32)*(5/9);
+  tempC = tempC.toFixed(1);
+  return parseFloat(tempC);
 }
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+// 9/5*C + 32 = F
+  let tempF = (9/5)*tempC + 32;
+  tempF = tempF.toFixed(1);
+  return parseFloat(tempF);
 }
 
 module.exports = {
