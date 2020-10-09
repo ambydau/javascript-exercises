007f147c209258216822c1dfa9794b539fed9991
Amber <amberedaniels09@gmail.com>
2020-10-09 15:29:47 -0700

    sumAll exercise completed

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+      let sum = 0;
+      // If a or b is negative or if a or b is NaN: return "ERROR"
+      if(a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)){
+            return "ERROR";
+      } else if(a < b){
+            for(let i = a; i <= b; i++){
+                  sum += i;
+                  
+            }
+      } else {
+            for(let i = b; i <= a; i++){
+                  sum += i;    
+            }
+      }
+      return sum;
 }
 
 module.exports = sumAll
